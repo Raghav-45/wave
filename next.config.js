d9a8cf1714f5cd1305082e0999801ebbb34702c1
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-03-27 22:28:35 +0530

    Added Chakra UI, Next-PWA, installed Supabase

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // reactStrictMode: true,
+  disable: process.env.NODE_ENV === 'development',
+});
